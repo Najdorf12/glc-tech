8e457d1afc6d8e28e046098e974de95564081695
Agustin Morro <agustin.morro@gmail.com>
2024-05-30 01:40:20 -0300

    change border cards

--- src/components/CardProductsAdmin.jsx
+++ src/components/CardProductsAdmin.jsx
@@ -25,20 +25,20 @@ const CardProductsAdmin = ({ product, getProducts }) => {
           <p className="text-lg  font-text font-semibold text-[#f1a415]">
             {name.toUpperCase()}
           </p>
-          <p className="mt-1 text-sm  font-text text-zinc-400 leading-6">
+          <p className="mt-1 text-sm  font-text text-gray-200 leading-6">
             {description}
           </p>
           <div className="flex  justify-between pr-4  w-full items-center mt-2">
-            <span className="font-text  font-base text-md  text-zinc-300  tracking-wide rounded-lg mb-1">
+            <span className="font-text  font-base text-md  text-gray-500  tracking-wide rounded-lg mb-1">
               {category}
             </span>
-            <p className="py-1 px-4 border border-[#f1a415] font-semibold font-title text-gray-300 rounded-md">
+            <p className="py-1 px-4 border border-[#92856e] font-semibold font-title text-[#92856e] rounded-md">
               $USD {price}
             </p>
             <section>
               <i
                 onClick={() => deleteProduct(_id)}
-                className="bx bxs-trash-alt text-3xl  text-[#f1a415] cursor-pointer hover:scale-110 hover:text-zinc-900 duration-300"
+                className="bx bxs-trash-alt text-3xl  text-[#f1a415] cursor-pointer hover:scale-110 hover:text-gray-100 duration-300"
               ></i>
             </section>
           </div>
