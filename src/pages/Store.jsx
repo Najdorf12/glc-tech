406a7fa4a9286cc336221bdf88aee315ddbd7e04
Agustin Morro <agustin.morro@gmail.com>
2024-06-02 17:32:56 -0300

    fix cards movile

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -36,7 +36,7 @@ const Store = () => {
     <>
       <section
         id="store"
-        className=" relative w-full pb-24 bg-zinc-900  flex flex-wrap  gap-6 justify-center pt-52 sm:pt-56  lg:pt-52 lg:px-10 lg:gap-8 lg:pb-32 2xl:gap-10 2xl:px-20"
+        className=" relative w-full pb-24 bg-zinc-900  flex flex-wrap  gap-2 sm:gap-3 justify-center pt-52 sm:pt-56  lg:pt-52 lg:px-10 lg:gap-8 lg:pb-32 2xl:gap-10 2xl:px-20"
       >
         <div className="absolute top-0 flex flex-wrap justify-center items-center gap-5 mt-10 px-2 sm:mt-14 lg:gap-10 2xl:gap-14">
           {btns.map((btn, i) => (
