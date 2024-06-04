c8fa70aa5e8a98bd61e82f39c7f0f1c242d397d7
Agustin Morro <agustin.morro@gmail.com>
2024-06-04 19:00:07 -0300

    fix h cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,27 +4,27 @@ import { Link } from "react-router-dom";
 const Card = ({ product }) => {
   return (
     <>
-      <div className="card-container w-[175px] h-[18.8rem] sm:w-[190px] sm:h-[20.5rem] md:w-[260px] md:h-[26.3rem] md: hover:scale-105 duration-500">
+      <div className="card-container w-[175px] h-[18.9rem] sm:w-[190px] sm:h-[19.5rem] md:w-[260px] md:h-[26.3rem] md: hover:scale-105 duration-500">
         <picture className="w-full">
           <Link to={`/${product._id}`}>
             <img
-              className="rounded-t-lg w-full md:h-[260px] object-cover"
+              className="rounded-t-lg w-full h-[170px] md:h-[260px] object-cover"
               src={product.image?.secure_url}
               alt=""
             />
           </Link>
         </picture>
-        <article className="relative flex flex-col gap-1 pt-2 pl-2 font-title">
-          <h6 className="text-[#f1a415] font-semibold text-sm sm:text-[0.9rem] md:text-lg">
+        <article className="relative flex flex-col   gap-1 pt-2 pl-2 font-title">
+          <h6 className="text-[#f1a415] font-semibold leading-4 text-sm sm:text-[0.9rem] md:text-lg">
             {product.name.toUpperCase()?.substring(0, 21)}
           </h6>
           <p className="text-[#92856e] font-semibold text-sm sm:text-[0.9rem] md:text-base">
             {product.category}
           </p>
           <p className="text-gray-200 text-xs sm:text-sm pr-1 mt-1 font-medium md:text-[0.9rem]">
-            {product.description?.substring(0, 25)}
+            {product.description?.substring(0, 20)}
           </p>
-          <button className="w-[85px] md:w-[110px] border border-[#92856e] self-start mt-2 sm:mt-4 lg:mt-3  rounded-[6px] font-title  font-medium  text-[#92856e] hover:scale-105 duration-500">
+          <button className=" w-[85px] md:w-[110px] border border-[#92856e] self-start mt-2 sm:mt-3 lg:mt-3  rounded-[6px] font-title  font-medium  text-[#92856e] hover:scale-105 duration-500">
             <a
               className="flex items-center  pl-2 gap-1 text-xs md:text-base"
               href="https://api.whatsapp.com/send/?phone=541125043539"
