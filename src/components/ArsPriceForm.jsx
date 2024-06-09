f004d1ffc0b5b7b0b92b8f379d6748c299c88c71
Agustin Morro <agustin.morro@gmail.com>
2024-06-09 00:03:30 -0300

    add Ars Usd Converter

--- src/components/ArsPriceForm.jsx
+++ src/components/ArsPriceForm.jsx
@@ -22,23 +22,30 @@ const ArsPriceForm = () => {
   }, []);
 
   const submit = (data) => {
+    console.log(data);
     const newArsPrice = {
       usdPrice: data.arsPrice,
     };
     axios
       .post("https://glctech-backend.onrender.com/api/usdprice", newArsPrice)
+      .then(() => getARSPrice())
       .catch((error) => console.error(error));
   };
-  const deleteArsPrice = (id) => {
+  const deleteArsPrice = () => {
     axios
-      .delete(`https://glctech-backend.onrender.com/api/usdprice/${id}`)
+      .delete(
+        `https://glctech-backend.onrender.com/api/usdprice/${ARSPrice[0]._id}`
+      )
+      .then(() => getARSPrice())
       .catch((error) => console.error(error));
   };
   return (
     <section className="form-container mt-5">
       <form onSubmit={handleSubmit(submit)}>
         <div className="form-group">
-          <label htmlFor="arsPrice">Peso Argentino - ACTUAL : {ARSPrice[0]?.usdPrice}</label>
+          <label htmlFor="arsPrice">
+            Peso Argentino - ACTUAL : {ARSPrice[0]?.usdPrice}
+          </label>
           <input name="arsPrice" id="arsPrice" {...register("arsPrice")} />
         </div>
         <div className="flex justify-between items-center mt-2">
@@ -46,7 +53,7 @@ const ArsPriceForm = () => {
             Subir nuevo
           </button>
           <button
-            onClick={() => deleteArsPrice(ARSPrice[0]?._id)}
+            onClick={deleteArsPrice}
             className="py-2 px-3 bg-zinc-900 text-zinc-300 border border-[#f1a415]  mt-2 rounded-lg font-text text-base  "
           >
             Eliminar Ultimo
