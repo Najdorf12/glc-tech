7cc42974c601c7826c5d6b10bb2c8ca8eb53717b
Agustin Morro <agustin.morro@gmail.com>
2024-06-19 18:27:55 -0300

    fix prod detail layout

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -30,19 +30,19 @@ const ProductDetail = () => {
 
   return (
     <section className="relative bg-zinc-900 pb-12 xl:h-dvh w-full pt-6 lg:pt-12 flex flex-col items-center gap-14  lg:justify-center xl:flex-row  xl:gap-24 2xl:gap-40 overflow-hidden">
-      <article className="font-title flex flex-col justify-center items-center gap-3 xl:gap-4">
+      <article className="font-title flex flex-col justify-center items-center gap-3 xl:gap-3">
         <h5 className="text-3xl text-[#92856e] font-semibold lg:self-start xl:text-4xl 2xl:text-5xl">
           {productDetail.name}
         </h5>
         <p className="text-center lg:text-start text-lg text-zinc-400 font-semibold  sm:max-w-[500px] lg:self-start xl:max-w-[420px] md:text-xl  2xl:max-w-[470px] ">
           {productDetail.description}
         </p>
-        <p className="text-center lg:text-start text-base text-zinc-300 sm:px-3 lg:self-start lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px] 2xl:mt-3">
+        <p className="text-center lg:text-start text-base text-zinc-300 sm:px-3 lg:self-start lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px] 2xl:mt-2">
           {productDetail.description2}
         </p>
-        <div className="my-3 flex justify-center items-center lg:self-start lg:py-4">
+        <div className="my-3 flex justify-center items-center lg:justify-start lg:my-[3%] 2xl:bg-red-500 2xl:self-start">
           <iframe
-            className="aspect-video w-full h-[250px]"
+            className="aspect-video w-full h-[250px]  xl:h-[230px]"
            
             src={
               productDetail.youtube?.replace("watch?v=", "embed/")?.split("&")[0]
@@ -52,7 +52,7 @@ const ProductDetail = () => {
           ></iframe>
         </div>
         <div className="flex justify-center items-center gap-3 sm:gap-10 lg:self-start">
-          <p className="mt-2 border border-[#92856e] py-2 px-4 rounded-lg text-xl font-bold text-[#92856e] 2xl:text-2xl 2xl:px-7 md:tracking-wide">
+          <p className="mt-2  border border-[#92856e] py-2 px-4 rounded-lg text-xl font-bold text-[#92856e] 2xl:text-2xl 2xl:px-7 md:tracking-wide">
             $USD {productDetail.price}
           </p>
           <p className="mt-2 border border-[#92856e] py-2 px-4 rounded-lg text-xl font-bold text-[#92856e] 2xl:text-2xl 2xl:px-7 md:tracking-wide">
@@ -60,7 +60,7 @@ const ProductDetail = () => {
           </p>
         </div>
       </article>
-      <picture className="w-[300px] sm:w-[350px] md:w-[400px] xl:w-[450px]">
+      <picture className="w-[300px] sm:w-[350px] md:w-[400px] xl:w-[400px] ">
         <img
           className="w-full rounded-lg"
           src={productDetail.image?.secure_url}
@@ -68,12 +68,12 @@ const ProductDetail = () => {
         />
       </picture>
       <Link to={"/"}>
-        <div className=" flex justify-center items-center text-xl font-title font-bold text-[#92856e] xl:absolute xl:bottom-6 xl:left-12 xl:text-3xl">
+        <div className=" flex justify-center items-center text-xl font-title font-bold text-[#92856e] xl:absolute xl:bottom-[0] xl:right-0 xl:left-0 xl:text-3xl">
           <i class="bx bx-left-arrow-alt text-4xl xl:text-6xl"></i>
           Home
         </div>
       </Link>
-      <div className="text-6xl font-title font-bold text-[#92856e] xl:absolute  xl:top-0 xl:right-0 xl:mt-6 xl:mr-6">
+      <div className="text-6xl font-title font-bold text-[#92856e] xl:absolute  xl:top-0 xl:right-0 xl:mt-[5px] xl:mr-6">
         GLC TECH
       </div>
     </section>
