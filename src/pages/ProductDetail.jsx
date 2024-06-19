ebcba9e078d26b46cfdfb5b0d3eecb4eed01398e
Agustin Morro <agustin.morro@gmail.com>
2024-06-19 13:36:39 -0300

    add youtube

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,23 +1,24 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "axios";
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [productDetail, setProductDetail] = useState({});
-  const [ARSPrice,setARSPrice] = useState([])
+  const [ARSPrice, setARSPrice] = useState([]);
 
   useEffect(() => {
     getProductDetail();
     getARSPrice();
   }, []);
-console.log(productDetail)
+  console.log(productDetail.youtube);
   const getARSPrice = () => {
     axios
-    .get("https://glctech-backend.onrender.com/api/usdPrice")
-    .then((res) => setARSPrice(res.data))
-    .catch((error) => console.error(error));
-  }
+      .get("https://glctech-backend.onrender.com/api/usdPrice")
+      .then((res) => setARSPrice(res.data))
+      .catch((error) => console.error(error));
+  };
   const getProductDetail = () => {
     axios
       .get(`https://glctech-backend.onrender.com/api/products/${id}`)
@@ -36,20 +37,26 @@ console.log(productDetail)
         <p className="text-center lg:text-start text-lg text-zinc-400 font-semibold  sm:max-w-[500px] lg:self-start xl:max-w-[420px] md:text-xl  2xl:max-w-[470px] ">
           {productDetail.description}
         </p>
-        <p className="text-center lg:text-start text-base text-zinc-300 sm:px-3 lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px] 2xl:mt-3">
+        <p className="text-center lg:text-start text-base text-zinc-300 sm:px-3 lg:self-start lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px] 2xl:mt-3">
           {productDetail.description2}
         </p>
-        <p className="text-center lg:self-start  ">
-          <Link to={`${productDetail.youtube}`}>
-             <i className='bx bxl-youtube text-5xl sm:text-5xl text-[#FF0000]'></i>
-          </Link>
-        </p>
+        <div className="my-3 flex justify-center items-center lg:self-start lg:py-4">
+          <iframe
+            className="aspect-video w-full h-[250px]"
+           
+            src={
+              productDetail.youtube?.replace("watch?v=", "embed/")?.split("&")[0]
+            }
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </div>
         <div className="flex justify-center items-center gap-3 sm:gap-10 lg:self-start">
           <p className="mt-2 border border-[#92856e] py-2 px-4 rounded-lg text-xl font-bold text-[#92856e] 2xl:text-2xl 2xl:px-7 md:tracking-wide">
             $USD {productDetail.price}
           </p>
           <p className="mt-2 border border-[#92856e] py-2 px-4 rounded-lg text-xl font-bold text-[#92856e] 2xl:text-2xl 2xl:px-7 md:tracking-wide">
-          $ {productDetail.price * ARSPrice[0]?.usdPrice}
+            $ {productDetail.price * ARSPrice[0]?.usdPrice}
           </p>
         </div>
       </article>
@@ -61,12 +68,12 @@ console.log(productDetail)
         />
       </picture>
       <Link to={"/"}>
-        <div className=" flex justify-center items-center text-xl font-title font-bold text-[#92856e] xl:absolute xl:bottom-12 xl:left-12 xl:text-3xl">
+        <div className=" flex justify-center items-center text-xl font-title font-bold text-[#92856e] xl:absolute xl:bottom-6 xl:left-12 xl:text-3xl">
           <i class="bx bx-left-arrow-alt text-4xl xl:text-6xl"></i>
           Home
         </div>
       </Link>
-      <div className="text-6xl font-title font-bold text-[#92856e] xl:absolute  xl:top-0 xl:left-0 xl:mt-6 xl:ml-6">
+      <div className="text-6xl font-title font-bold text-[#92856e] xl:absolute  xl:top-0 xl:right-0 xl:mt-6 xl:mr-6">
         GLC TECH
       </div>
     </section>
