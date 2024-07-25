fffde59790c820f22d2c07c41e146f74bc3db1f1
Najdorf12 <agustin.morro@gmail.com>
2024-07-25 20:31:49 -0300

    add route

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const instance= axios.create({
-<<<<<<< HEAD
-    baseURL:"https://glc-tech-backend.vercel.app/",
-=======
-    baseURL:"https://www.grupolacomunidad.com.ar/api",
->>>>>>> c8a6e7753971ab0b2bbce9f1c9206490cac697c9
+    baseURL:"https://glc-tech-backend.vercel.app/api",
     withCredentials: "true"
 })
 export default instance
