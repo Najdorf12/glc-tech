8f9ed16644c414f4a1ac609d43b7f53dc7022b88
Najdorf12 <agustin.morro@gmail.com>
2024-07-26 11:28:11 -0300

    login

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance= axios.create({
-    baseURL:"http://localhost:1212/api",
+    baseURL:" https://glc-tech-backend.vercel.app/api",
     withCredentials: "true",
 
 })
 export default instance
 
-/* https://glc-tech-backend.vercel.app/api */
+/*http://localhost:1212/api*/
