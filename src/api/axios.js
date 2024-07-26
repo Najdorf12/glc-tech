5a260b4b13ec6e11463499391bc9c7948df92d68
Najdorf12 <agustin.morro@gmail.com>
2024-07-26 13:33:15 -0300

    fix protected routes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance= axios.create({
-    baseURL:"http://localhost:1212/api",
+    baseURL:"https://glc-tech-backend.vercel.app/api",
     withCredentials: "true",
 })
 export default instance
 
-/* http://localhost:1212/api 
+/* https://glc-tech-backend.vercel.app/api 
 
 http://localhost:1212/api*/
