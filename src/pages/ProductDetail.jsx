15c128283af69f3d5779d9304e653f5b4d466afb
Najdorf12 <agustin.morro@gmail.com>
2024-07-30 16:23:04 -0300

    add styles

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -34,9 +34,9 @@ const ProductDetail = () => {
   };
 
   return (
-    <section className="relative bg-gray-300 pb-12  2xl:h-dvh w-full pt-3 lg:pt-12 flex flex-col items-center   lg:justify-center xl:flex-row  overflow-hidden">
-      <nav className="w-full flex items-center justify-between px-3">
-        <ul className="text-[#838282ab] text-base flex  pl-3 items-center font-semibold">
+    <section className="relative bg-gray-300 pb-12  w-full pt-3  flex flex-col items-center  overflow-hidden xl:pt-4 2xl:min-h-screen">
+      <nav className="w-full flex items-center justify-between px-3 xl:px-8 2xl:px-12 ">
+        <ul className="text-gray-500 text-base flex  pl-3 items-center font-semibold 2xl:text-lg">
           <li>Todos</li>
           <li>
             <i className="bx bx-chevron-right text-3xl mt-1"></i>
@@ -44,79 +44,97 @@ const ProductDetail = () => {
           <li>{productDetail.name}</li>
         </ul>
         <Link to={"/"}>
-          <button className="flex items-center text-[#838282ab] text-base font-semibold border-[2px] rounded-[1rem] px-5 py-1 border-white">
+          <button className="flex items-center text-gray-500 text-base font-semibold border-[2px] rounded-[1rem] px-5 py-1 border-white mt-[3px] xl:px-7 2xl:text-lg">
             Volver
           </button>
         </Link>
       </nav>
       {isLoading && <Loader />}
-      <article className="font-title flex flex-col justify-center items-center gap-3 xl:gap-3  mt-10">
-        <h5 className="text-3xl text-stone-500 font-bold lg:self-start xl:text-4xl 2xl:text-5xl">
-          {productDetail.name}
-        </h5>
-        <div className="flex justify-center items-center gap-3 sm:gap-10 lg:self-start">
-          <p className=" py-2 px-4 text-2xl font-bold text-stone-600 2xl:text-2xl 2xl:px-7 md:tracking-wide">
-            $USD {productDetail.price}
-          </p>
-          <p className=" py-2 px-4  text-2xl font-bold text-stone-600 2xl:text-2xl 2xl:px-7 md:tracking-wide">
-            $ {productDetail.price * ARSPrice[0]?.usdPrice}
+      <section className=" p-1 xl:flex xl:justify-evenly xl:w-[90%] 2xl:w-[80%] xl:mt-4 2xl:mt-10 2xl:py-6">
+        <div className="flex flex-col justify-center items-center  xl:justify-start">
+          <article className=" font-title flex flex-col justify-center items-center gap-3 xl:gap-3  mt-10 xl:mt-2">
+            <h5 className="text-3xl text-stone-500 font-bold 2xl:text-4xl">
+              {productDetail.name}
+            </h5>
+            <div className="flex justify-center items-center gap-3 sm:gap-10 ">
+              <p className=" py-2 px-4 text-2xl font-bold text-stone-600 2xl:text-3xl 2xl:px-7 md:tracking-wide">
+                $USD {productDetail.price}
+              </p>
+              <p className=" py-2 px-4  text-2xl font-bold text-stone-600 2xl:text-3xl 2xl:px-7 md:tracking-wide">
+                $ {productDetail.price * ARSPrice[0]?.usdPrice}
+              </p>
+            </div>
+            <button className="btn-home2 mt-1 py-2 px-6 xl:py-[9px] xl:px-12 hover:scale-105 hover:duration-500 tracking-wider font-title font-bold text-stone-500">
+              <span>COMPRAR</span>
+            </button>
+          </article>
+
+          <picture className="mt-5 xl:mt-8 2xl:mt-12">
+            <img
+              className="w-full rounded-lg max-w-[350px] 2xl:max-w-[400px]"
+              src={productDetail.image?.secure_url}
+              alt=""
+            />
+          </picture>
+        </div>
+        <div className="flex flex-col  xl:justify-center xl:gap-6 xl:pt-10">
+          <div className="mt-4 w-full flex justify-center text-sm text-stone-600 font-semibold font-title 2xl:text-base">
+            <ul className="rounded-lg w-[90%] flex flex-col gap-3 py-2  max-w-[500px] ">
+              <li className="flex items-center justify-between border-[2px] border-white py-[6px]  px-2 rounded-xl 2xl:px-3">
+                <div className="flex gap-2 items-center text-gray-500">
+                  Procesador
+                </div>
+                <div>
+                  {productDetail.procesador
+                    ? productDetail.procesador
+                    : productDetail.description}
+                </div>
+              </li>
+              <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
+                <div className="flex gap-2 items-center text-gray-500">
+                  Cámara
+                </div>
+                <div>
+                  {productDetail.camara
+                    ? productDetail.camara
+                    : "FRONTAL 5MP - TRASERA 8MP"}
+                </div>
+              </li>
+              <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
+                <div className="flex gap-2 items-center text-gray-500">
+                  Pantalla
+                </div>
+                <div>
+                  {productDetail.pantalla
+                    ? productDetail.pantalla
+                    : "PANTALLA FLUIDA DE 6,71 Y 90 HZ"}
+                </div>
+              </li>
+              <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
+                <div className="flex gap-2 items-center text-gray-500">
+                  Memoria
+                </div>
+                <div>{productDetail.description}</div>
+              </li>
+              <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
+                <div className="flex gap-2 items-center text-gray-500">
+                  Batería
+                </div>
+                <div>
+                  {productDetail.bateria ? productDetail.bateria : "5000MAH"}
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <p className="text-center  text-base font-semibold font-title text-stone-600 mt-4 px-3 sm:px-4  lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px]">
+            {productDetail.description2}
           </p>
         </div>
-        <button className="btn-home2 mt-1 py-2 px-6 xl:py-[9px] xl:px-12 hover:scale-105 hover:duration-500 tracking-wider font-title font-bold text-stone-500">
-          <span>COMPRAR</span>
-        </button>
-      </article>
-      <picture className="mt-5 w-[300px] sm:w-[350px] md:w-[400px] xl:w-[400px]  ">
-        <img
-          className="w-full rounded-lg"
-          src={productDetail.image?.secure_url}
-          alt=""
-        />
-      </picture>
-      <div className="mt-4 w-full flex justify-center text-sm text-stone-600 font-semibold font-title">
-        <ul className="rounded-lg w-[90%] flex flex-col gap-3 py-2  ">
-          <li className="flex items-center justify-between border-[2px] border-white py-[6px]  px-2 rounded-xl">
-            <div className="flex gap-2 items-center text-gray-500">Procesador</div>
-            <div>
-              {productDetail.procesador
-                ? productDetail.procesador
-                : productDetail.description}
-            </div>
-          </li>
-          <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
-            <div className="flex gap-2 items-center text-gray-500">Cámara</div>
-            <div>
-              {productDetail.camara
-                ? productDetail.camara
-                : "FRONTAL 5MP - TRASERA 8MP"}
-            </div>
-          </li>
-          <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
-            <div className="flex gap-2 items-center text-gray-500">Pantalla</div>
-            <div>
-              {productDetail.pantalla
-                ? productDetail.pantalla
-                : "PANTALLA FLUIDA DE 6,71 Y 90 HZ"}
-            </div>
-          </li>
-          <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
-            <div className="flex gap-2 items-center text-gray-500">Memoria</div>
-            <div>{productDetail.description}</div>
-          </li>
-          <li className="flex items-center justify-between border-[2px] border-white py-[6px] px-2 rounded-xl">
-            <div className="flex gap-2 items-center text-gray-500">Batería</div>
-            <div>
-              {productDetail.bateria ? productDetail.bateria : "5000MAH"}
-            </div>
-          </li>
-        </ul>
-      </div>
-      <p className="text-center lg:text-start text-base font-semibold font-title text-stone-600 mt-4 px-3 sm:px-4 lg:self-start lg:px-0 sm:max-w-[500px] xl:max-w-[420px] 2xl:text-lg 2xl:leading-6 2xl:max-w-[470px] 2xl:mt-2">
-        {productDetail.description2}
-      </p>
-      <div className="my-3 mt-5 flex justify-center items-center lg:justify-start lg:my-[3%] 2xl:bg-teal-800 2xl:self-start">
+      </section>
+      <div className="my-3 mt-5 flex justify-center items-center xl:mt-20 xl:w-[600px] xl:self-center 2xl:w-[600px]">
         <iframe
-          className="aspect-video w-full h-[250px]  xl:h-[230px]"
+          className="aspect-video w-full h-[250px]  xl:h-[300px] rounded-lg"
           src={
             productDetail.youtube?.replace("watch?v=", "embed/")?.split("&")[0]
           }
