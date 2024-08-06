7b7bbd6dc4899905c188628c92d449cc9b2e399a
Najdorf12 <agustin.morro@gmail.com>
2024-08-06 20:12:29 -0300

    add styles

--- src/components/CardSimilarProd.jsx
+++ src/components/CardSimilarProd.jsx
@@ -1,20 +1,24 @@
 const CardSimilarProd = ({ product }) => {
   return (
-    <div className=" w-44 h-[250px] 2xl:w-60 2xl:h-[320px] bg-stone-600 p-3 flex flex-col gap-1 rounded-br-3xl">
+    <div className=" w-40 h-[256px] 2xl:w-60 2xl:h-[320px] p-2 flex flex-col gap-1 rounded-br-3xl rounded-t-lg">
       <div className="w-full flex justify-center items-center ">
-        <img src={product?.image?.secure_url} 
-        alt="img-similar-prod" 
-        className="w-32 2xl:w-48 rounded-lg"
+        <img
+          src={product?.image?.secure_url}
+          alt="img-similar-prod"
+          className="w-36 2xl:w-48 rounded-lg"
         />
       </div>
       <div className="flex flex-col gap-1">
-        <span className="text-base 2xl:text-xl text-white font-bold">{product?.name} </span>
+        <span className="text-base 2xl:text-xl text-white font-bold">
+        {product?.name?.toUpperCase()?.substring(0, 14)}
+        </span>
         <div className="flex flex-row justify-between items-center">
-          <p className="text-sm font-semibold text-stone-300">{product?.description}</p>
-          
+          <p className="text-sm font-semibold text-gray-500">
+            {product?.description}
+          </p>
         </div>
-        <button className="hover:bg-amber-600 mt-1 text-gray-100 font-medium bg-[#92856e] py-2 rounded-br-xl">
-            Ver detalle
+        <button className="hover:bg-amber-600 w-full mt-[5px] text-gray-100 font-medium bg-[#bd8f2db4] py-1 rounded-br-xl self-start">
+          {product?.price} USD
         </button>
       </div>
     </div>
