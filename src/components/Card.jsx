36fbf524ff16c0cb467f5e756a1a41c674847a60
Najdorf12 <agustin.morro@gmail.com>
2024-08-07 20:51:51 -0300

    add styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Card = ({ product, arsPrice }) => {
   return (
     <>
-      <div className="card-container w-[180px] h-[19.55rem] sm:w-[192px] sm:h-[19.80rem] md:w-[220px] md:h-[26.5rem]  lg:w-[240px] lg:h-[26.6rem] xl:w-[250px] xl:h-[27.5rem] hover:scale-105 duration-500">
+      <div className="card-container w-[185px] h-[19.70rem] sm:w-[192px] sm:h-[19.85rem] md:w-[220px] md:h-[26.5rem]  lg:w-[240px] lg:h-[26.6rem] xl:w-[255px] xl:h-[27.6rem] hover:scale-105 duration-500">
         <Link to={`/${product._id}`}>
           <picture className="w-full">
             <img
@@ -16,19 +16,19 @@ const Card = ({ product, arsPrice }) => {
           </picture>
         </Link>
 
-        <article className="relative flex flex-col   gap-1 pt-2 font-title ">
-          <h6 className="text-gray-600 font-bold leading-4 text-base self-center sm:text-[1rem] md:text-lg lg:font-bold border-b w-[90%] pb-[4px] flex items-center justify-center">
+        <article className="relative flex flex-col   gap-1 pt-2 font-title">
+          <h6 className="text-stone-600 font-bold leading-4 text-base self-center sm:text-[1rem] md:text-lg lg:font-bold border-b w-[90%] pb-[4px] flex items-center justify-center">
             {product.name?.toUpperCase()?.substring(0, 18)}
           </h6>
-          <p className="text-stone-500 pl-3 font-semibold text-sm sm:mt-[1px] md:text-[0.9rem] md:text-base xl:mt-[3px]">
+          <p className="text-stone-500 pl-3 font-semibold text-sm sm:mt-[1px] md:text-[1rem] md:text-base xl:mt-[3px]">
             <Link to={`/${product._id}`}>{product.category}</Link>
           </p>
-          <p className="text-stone-500 pl-3 text-xs sm:text-[0.8rem] pr-1 sm:-mt-[.3px] md:text-[0.9rem] font-semibold">
+          <p className="text-stone-500 pl-3 text-sm sm:text-[0.8rem] pr-1 sm:-mt-[.3px] md:text-[1rem] font-semibold">
             <Link to={`/${product._id}`}>
               {product.description?.substring(0, 20)}
             </Link>
           </p>
-          <div className="flex gap-3 mt-[1px] sm:mt-[1.5px] self-center text-[.95rem] text-stone-600 font-bold md:text-[1rem] lg:text-[1.1rem] xl:my-1 ">
+          <div className="flex gap-3 mt-[1px] sm:mt-[1.5px] self-center text-[1rem] text-[#60438f] font-bold md:text-[1rem] lg:text-[1.2rem]  ">
             <p className="">
               <Link to={`/${product._id}`}>usd {product?.price}</Link>
             </p>
@@ -38,8 +38,7 @@ const Card = ({ product, arsPrice }) => {
               </Link>
             </p>
           </div>
-
-          <button className="w-[70%] md:w-[60%] mt-1  bg-gradient-to-br from-stone-500 to-stone-800  py-[2px] self-center rounded-[1.3rem] font-title font-semibold text-[.85rem] md:text-[.90rem] lg:text-[.98rem] text-white hover:scale-105 duration-500  xl:py-[2.7px] ">
+          <button className="bg-gradient-to-br from-[#051937] to-[#845EC2] w-[60%] mt-[3px] text-gray-100 text-[.9rem] font-medium py-[2px] rounded-full self-center lg:text-base lg:py-[5px] lg:mt-[5px] xl:font-semibold">
             <Link
               className="flex items-center  justify-center gap-2 "
               to={"https://api.whatsapp.com/send/?phone=541125043539"}
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -24,7 +24,7 @@ const ProductDetail = () => {
         : productDetail?.image?.secure_url,
     },
     {
-      original: productDetail?.images 
+      original: productDetail?.images
         ? productDetail?.images[1]?.secure_url
         : productDetail?.image?.secure_url,
       thumbnail: productDetail?.images
@@ -40,7 +40,7 @@ const ProductDetail = () => {
         : productDetail?.image?.secure_url,
     },
   ];
- 
+
   useEffect(() => {
     const fetchProductData = async () => {
       try {
@@ -77,10 +77,10 @@ const ProductDetail = () => {
           <li>
             <i className="bx bx-chevron-right text-3xl mt-1 font-light"></i>
           </li>
-          <li>{productDetail.name?.substring(0, 14)}</li>
+          <li>{productDetail.name?.toUpperCase()?.substring(0, 14)}</li>
         </ul>
         <Link to={"/"}>
-          <button className="btn-home2 flex items-center text-gray-500 text-base font-normal border-[2px] rounded-[1rem] px-5 py-1 border-white mt-[3px] xl:px-8 2xl:text-lg 2xl:px-8  xl:font-semibold">
+          <button className="btn-home2 flex items-center text-gray-500 text-base font-normal border-[2px] rounded-[1rem] px-5 py-1 border-white mt-[3px] xl:px-8 2xl:text-lg 2xl:px-8  xl:font-semibold ">
             Volver
           </button>
         </Link>
@@ -88,22 +88,25 @@ const ProductDetail = () => {
       {isLoading && <Loader />}
       <section className=" p-1 xl:flex xl:justify-evenly xl:w-[90%] 2xl:w-[80%] xl:mt-10  2xl:py-6">
         <div className="flex flex-col justify-center items-center  xl:justify-start">
-          <article className="font-title flex flex-col justify-center items-center  mt-10 xl:mt-2">
-            <h5 className="text-3xl text-stone-500 font-bold xl:text-4xl  py-2 px-8 2xl:text-5xl bg-gradient-to-t from-stone-700 to-stone-400 inline-block text-transparent bg-clip-text ">
-              {productDetail.name?.toUpperCase()}
+          <article id="title-product-detail" className="font-title flex flex-col justify-center items-center  mt-10 xl:mt-2   rounded-[1.5em] bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.25)] backdrop-blur-[12px] pt-3 pb-5 md:py-6">
+            <h5 className="text-3xl  font-bold xl:text-4xl  py-2 px-8 2xl:text-5xl bg-gradient-to-t from-stone-100 to-stone-400 inline-block text-transparent bg-clip-text ">
+              {productDetail.name?.toUpperCase()?.substring(0, 19)}
             </h5>
-            <div className="flex justify-center items-center gap-3 sm:gap-10 2xl:mt-3">
-              <p className=" py-2 px-4 text-3xl font-bold text-stone-600 xl:text-4xl 2xl:px-7 md:tracking-wide 2xl:text-5xl">
+            <div className="flex justify-center items-center gap-3 sm:gap-10 2xl:mt-3 ">
+              <p className=" py-2 px-4 text-3xl font-bold xl:text-4xl 2xl:px-7 md:tracking-wide 2xl:text-5xl bg-gradient-to-br from-white to-gray-300 inline-block text-transparent bg-clip-text">
                 $USD {productDetail.price}
               </p>
-              <p className=" py-2 px-4  text-3xl font-bold text-stone-600 xl:text-4xl 2xl:px-7 md:tracking-wide 2xl:text-5xl">
+              <p className=" py-2 px-4  text-3xl font-bold xl:text-4xl 2xl:px-7 md:tracking-wide 2xl:text-5xl bg-gradient-to-br from-white to-gray-300  inline-block text-transparent bg-clip-text">
                 $ {productDetail.price * ARSPrice[0]?.usdPrice}
               </p>
             </div>
-            <Link to="https://api.whatsapp.com/send/?phone=541125043539" target="blank">
-            <button className="btn-home2 mt-3 py-2 px-6 xl:py-[9px] xl:px-12 hover:scale-105 hover:duration-500 tracking-wider font-title font-bold text-stone-500 border-[2px] border-white 2xl:text-xl 2xl:mt-7">
-              <span>COMPRAR</span>
-            </button>
+            <Link
+              to="https://api.whatsapp.com/send/?phone=541125043539"
+              target="blank"
+            >
+              <button className="btn-home2 mt-3 py-2 px-6 xl:py-[9px] xl:px-12 hover:scale-105 hover:duration-500 tracking-wider font-title font-bold text-stone-100 border-[2px] border-white 2xl:text-xl 2xl:mt-7">
+                <span>COMPRAR</span>
+              </button>
             </Link>
           </article>
 
@@ -180,15 +183,14 @@ const ProductDetail = () => {
       <div className="my-3 w-full mt-7 flex justify-center items-center xl:mt-20 ">
         <iframe
           id="ytplayer"
-          type="text/html"
-         className="w-[98%] h-[270px] rounded-lg max-w-[500px] md:max-w-[600px] md:h-[330px] 2xl:max-w-[700px] 2xl:h-[350px]"
-           src={
-            productDetail.youtube?.replace("watch?v=", "embed/")?.split("&")[0] 
-          } 
+          className="w-[98%] h-[270px] rounded-lg max-w-[500px] md:max-w-[600px] md:h-[330px] 2xl:max-w-[700px] 2xl:h-[350px]"
+          src={
+            productDetail.youtube?.replace("watch?v=", "embed/")?.split("&")[0]
+          }
         ></iframe>
       </div>
 
-      <p className="text-stone-500 mb-5 2xl:mb-7 font-title font-bold text-lg mt-7 border-[2px] border-white py-[3px] px-7 rounded-xl  xl:mt-12 2xl:text-2xl 2xl:mt-16">
+      <p className="text-white mb-5 2xl:mb-7 font-title font-bold text-lg mt-7  py-[3px] px-7 rounded-xl  xl:mt-12 2xl:text-2xl 2xl:mt-16 bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.25)]">
         SIMILARES
       </p>
       <div className="w-full mt-2 gap-x-5 sm:gap-x-5 md:gap-6 xl:gap-10 2xl:gap-12 flex flex-wrap justify-center items-center ">
