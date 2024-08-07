4f5cac0676d4e66a152e259cbb7cdab36720c5b5
Najdorf12 <agustin.morro@gmail.com>
2024-08-07 15:07:42 -0300

    add styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ const Card = ({ product, arsPrice }) => {
 
         <article className="relative flex flex-col   gap-1 pt-2 font-title ">
           <h6 className="text-gray-600 font-bold leading-4 text-base self-center sm:text-[1rem] md:text-lg lg:font-bold border-b w-[90%] pb-[4px] flex items-center justify-center">
-            {product.name?.toUpperCase()?.substring(0, 21)}
+            {product.name?.toUpperCase()?.substring(0, 18)}
           </h6>
           <p className="text-stone-500 pl-3 font-semibold text-sm sm:mt-[1px] md:text-[0.9rem] md:text-base xl:mt-[3px]">
             <Link to={`/${product._id}`}>{product.category}</Link>
