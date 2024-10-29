217d20fdf4487c3e2bdc80b47633efcc7e103c49
Najdorf12 <agustin.morro@gmail.com>
2024-10-29 14:08:10 -0300

    add styles

---
+++ src/components/CardInfinix.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const CardInfinix = ({ product, arsPrice }) => {
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+  }, []);
+
+  return (
+    <>
+      <div className="card-container  w-[180px] h-[19.70rem] sm:w-[192px] sm:h-[19.85rem] md:w-[220px] md:h-[26.5rem]  lg:w-[240px] lg:h-[26.6rem] xl:w-[255px] xl:h-[27.6rem] hover:scale-105 duration-500 2xl:w-[270px] 2xl:h-[28rem]">
+        <Link to={`/${product?._id}`}>
+          <picture className="w-full ">
+            <img
+              loading="lazy"
+              className="rounded-xl rounded-b-none w-full h-[170px] md:h-[260px] object-cover border-[2px] border-b-0 border-stone-500"
+              src={product.image?.secure_url}
+              alt="phone-img"
+            />
+          </picture>
+        </Link>
+
+        <article className="relative flex flex-col  font-title  bg-gradient-to-tl from-teal-800 via-zinc-800 to-zinc-800 pt-3 pb-3 rounded-b-xl border-[2px] border-stone-500">
+          <h6 className="text-[rgb(146,195,73)]  font-bold leading-4 text-[1.05rem] self-start md:text-lg lg:font-bold lg:text-[1.3rem] 2xl:text-[1.40rem] w-full pl-2">
+            { windowWidth > 500
+              ? product.name?.toUpperCase()?.substring(0, 19)
+              : product.name?.toUpperCase()?.substring(0, 15)}
+          </h6>
+          <p className="text-stone-400  pl-2 font-bold text-[.95rem] mt-[4px]  md:text-[1.1rem] lg:text-[1.2rem] lg:mt-[2px] 2xl:text-[1.20rem] 2xl:mt-[3px]">
+            <Link to={`/${product._id}`}>{product.category}</Link>
+          </p>
+          <p className="text-stone-400  pl-2 text-[.95rem] sm:text-[0.95rem] pr-1  md:text-[1.1rem] lg:text-[1.2rem]  font-bold 2xl:text-[1.20rem]">
+            <Link to={`/${product._id}`}>
+              {product.description?.substring(0, 20)}
+            </Link>
+          </p>
+          <div className="flex  justify-between w-full px-2  mt-[4px] sm:mt-[1.5px] self-center text-[1.04rem]  text-[rgb(146,195,73)]  font-extrabold md:mt-[3px]  md:text-[1.25rem] lg:text-[1.5rem]   2xl:text-[1.50rem]">
+            <p className="">
+              <Link to={`/${product?._id}`}>usd {product?.price}</Link>
+            </p>
+            <p className="">
+              <Link to={`/${product._id}`}>
+                $ {product?.price * arsPrice[0]?.usdPrice}
+              </Link>
+            </p>
+          </div>
+          <button
+            className="bg-zinc-700 border-[2px] border-stone-500 w-[90%] ml-2 mt-[6px] text-stone-300 text-[1rem] font-semibold py-[3.5px] rounded-br-xl self-start md:text-[1.1rem] lg:py-[5px] md:mt-[9px] 2xl: xl:font-semibold xl:text-[1.25rem]  hover:text-[rgb(146,195,73)] hover:scale-105 hover:bg-white  duration-500"
+          >
+            <Link
+              className="flex items-center  justify-center gap-2 "
+              to={"https://api.whatsapp.com/send/?phone=541125043539"}
+              target="blank"
+            >
+              {" "}
+              COMPRAR
+              {/*  <i className="bx bxl-whatsapp text-[1.55rem] text-green-600 "></i> */}
+            </Link>
+          </button>
+        </article>
+      </div>
+    </>
+  );
+};
+
+export default CardInfinix;
--- src/pages/Infinix.jsx
+++ src/pages/Infinix.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
 import { Link } from "react-router-dom";
-import Card from "../components/Card";
+import CardInfinix from "../components/CardInfinix";
+import logoInfinix from "/InfinixLogo.png"
 
 const Infinix = ({ arsPrice }) => {
   const [allProductsInfinix, setAllProductsInfinix] = useState([]);
@@ -21,31 +22,30 @@ const Infinix = ({ arsPrice }) => {
 
   return (
     <>
-      <section
-        style={{
-          backgroundImage:
-            "linear-gradient(to right, #301b55, #291c4b, #231c40, #1e1b35, #1b1a2a, #1a1925, #191920, #18181b, #18181b, #18181b, #18181b, #18181b)",
-        }}
-        className="min-h-screen w-full overflow-hidden bg-zinc-800 flex flex-col text-white gap-2"
+      <section className="w-full min-h-screen relative flex flex-col justify-start pb-20 xl:pb-48 2xl:pb-64"
       >
-        <nav className="w-full flex items-center justify-end px-1 xl:px-8 2xl:px-12 2xl:pt-2 ">
+        <nav className="w-full flex items-center justify-end px-1 xl:px-8 2xl:px-12 2xl:pt-2 lg:justify-end fixed">
           <Link to={"/"}>
-            <button className="btn-home2 flex items-center text-stone-500 dark:text-gray-100 text-base font-normal font-title mt-2 border-[2px] rounded-[1rem] px-6 border-white xl:px-8 2xl:text-lg 2xl:px-8  xl:font-semibold hover:scale-105 ">
-              Volver
+            <button className="btn-home2 flex items-center bg-zinc-700 text-[rgb(146,195,73)]  text-base font-medium  font-title mt-2 border-[2px] rounded-[1rem] px-6 border-zinc-600 xl:px-8 2xl:text-lg 2xl:px-8  xl:font-semibold hover:scale-105 shadow-lg shadow-zinc-400 ">
+              IR A LA TIENDA
             </button>
           </Link>
         </nav>
-        <article className="flex flex-col items-center mt-2 gap-4 text-balance text-center">
-          <h5 className="text-5xl font-title font-semibold">INFINIX</h5>
-          <p className="text-stone-300  text-base font-medium px-3">
+       
+        <article className="flex flex-col items-center mt-2 gap-4 text-balance text-center xl:mt-12 2xl:mt-20">
+          <figure className="w-[90%] lg:w-[40%] xl:w-[45%]">
+          <img src={logoInfinix} alt="logoInfinix" className="w-full" />
+
+          </figure>
+          <p className="text-stone-500  text-base font-semibold px-3 leading-4 font-title lg:text-lg xl:text-xl 2xl:text-2xl">
             Aquí encontraras todos nuestros dispositivos Infinix.
             {/*   <i className="bx bx-log-in-circle text-3xl  text-white 2xl:text-4xl"></i> */}
           </p>
         </article>
 
-        <section className="mt-6">
+        <section className="mt-6 flex flex-wrap justify-center items-center gap-x-3 gap-y-12 px-2 md:px-12 lg:px-12 lg:gap-y-20 xl:gap-x-6  xl:mt-14 2xl:mt-16 2xl:px-[15%]">
           {allProductsInfinix?.map((productInfinix, i) => (
-            <Card key={i} product={productInfinix} arsPrice={arsPrice} />
+            <CardInfinix key={i} product={productInfinix} arsPrice={arsPrice} />
           ))}
         </section>
       </section>
