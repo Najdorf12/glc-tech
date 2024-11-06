97011e50cfc64bc3d8cd7262e44306bf06ecc075
Najdorf12 <agustin.morro@gmail.com>
2024-11-06 17:46:40 -0300

    new front

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,27 +4,27 @@ const Review = ({ imagesData }) => {
   /* #AB2343 */
   const testimonials = [
     {
-      name: "Marcos Lorem",
+      name: "Erick Machado",
       content:
         "La mejor compra que he hecho. Precio súper cómodo y con garantía. Buena atención.  Siempre me sentí seguro durante la compra. Yo compré el Moto G84. Los recomiendo porque tienen mucha variedad y al precio que vale, no a los que otras compañías más grandes te arrancan la cabeza. A mucho más de lo que vale, realmente. Le pongo 5 estrellas.",
     },
     {
-      name: "Enrique Lorem",
+      name: "Gerardo César Vallet",
       content:
         "Excelente y desinteresada atención. Asesoran, explican, sugieren. Los precios son los mejores por lejos.  Son muy buena gente y honesta por eso los recomiendo. Además, venden accesorios a muy buenos precios. Ofrecen servicio técnico y salimos con el teléfono listo. SON MUY RECOMENDABLES.",
     },
     {
-      name: "Juan Pablo Lorem",
+      name: "Brian Ulla",
       content:
         "Excelente servicio y atención. Soy del interior y estaba dudando por la compra , y si el producto iba a llegar a destino, la verdad que me sorprendió el precio del producto, por debajo de varios lugares donde estuve averiguando. El tema de la atención, impecable, te das cuenta del compromiso que tienen con la gente.",
     },
     {
-      name: "Gabriel Gonzalez",
+      name: "Claudio Javier Paludi",
       content:
-        "Quería agradecer el excelente asesoramiento y atención.Además de recomendar a Grupo La Comunidad a todas aquellas personas que estén buscando cambio de celular, que es lo que yo hice, pero dudaba en donde comprar. Equipos nuevos en caja cerrada y con garantía simplemente.",
+        "Quería agradecer el excelente asesoramiento y atención. Además de recomendar a Grupo La Comunidad a todas aquellas personas que estén buscando cambio de celular, que es lo que yo hice, pero dudaba en donde comprar. Equipos nuevos en caja cerrada y con garantía simplemente.",
     },
     {
-      name: "Soledad Martinez",
+      name: "Marcela Barzola",
       content:
         "Mi esposo compró un equipo con todos los temores de tener que depositar dinero (hoy todo es mucho) y esperar el envio. Pero quedó más que satisfecho. Recibió el t.e. en tiempo pactado, asesoramiento telefónico por parte del vendedor. La verdad, totalmente recomendable.Excelentes precios",
     },
