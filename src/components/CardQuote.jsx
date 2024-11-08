8d006252c76313f657faeb2080d62c538a03e5f0
Najdorf12 <agustin.morro@gmail.com>
2024-11-08 17:23:15 -0300

    add styles

--- src/components/CardQuote.jsx
+++ src/components/CardQuote.jsx
@@ -4,12 +4,12 @@ import quote from "/quote1.png";
 const CardQuote = ({testimonial}) => {
   return (
     <>
-      <div className="w-[480px] h-[360px] bg-gradient-to-br from-pink-800 via-pink-900 to-zinc-800 relative rounded-xl xl:w-[520px] xl:h-[380px] 2xl:w-[550px] 2xl:h-[430px]">
+      <div className="w-[520px] h-[380px]  relative rounded-xl xl:w-[720px] xl:h-[430px]">
        {/*  <figure className="w-20 mt-2 ml-2 xl:w-28">
           <img src={quote} alt="quote-icon" className="w-full" />
         </figure> */}
         <figure className="w-full h-full">
-          <img src={testimonial?.content} alt="quote-icon" className="w-full h-full object-cover rounded-lg border-[2px] border-stone-600 " />
+          <img src={testimonial?.content} alt="quote-icon" className="w-full h-full object-cover object-center xl:object-[center_-20px] rounded-lg border-[2px] border-stone-600 " />
         </figure>
        {/*  <article className="flex flex-col justify-center items-center text-balance text-center px-6  mt-3">
           <p className="z-50 text-sm text-zinc-400 font-title xl:text-base 2xl:text-xl">{testimonial?.content}</p>
