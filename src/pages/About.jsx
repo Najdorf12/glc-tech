3cc7ecfe09288a2bcde92401b935097f80b902e9
Najdorf12 <agustin.morro@gmail.com>
2024-11-08 18:01:32 -0300

    add styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,7 +25,7 @@ const About = ({imagesData}) => {
 
   return (
     <>
-      <section className="relative w-full h-[65vh]  z-10 flex justify-center items-center  text-gray-300  bg-white  lg:h-[80vh] lg:justify-end xl:items-start  xl:pt-[5%] xl:h-[90vh]  ">
+      <section className="relative w-full h-[65vh]  z-10 flex justify-center items-center  text-gray-300  bg-white  lg:h-[80vh] lg:justify-end xl:items-start  xl:pt-[7%] xl:h-[90vh]  ">
         <div className="h-full w-full  absolute inset-0 z-30 flex items-start justify-center  xl:justify-start">
           <figure className="w-full h-full z-30 flex justify-center items-center grayscale-[.0] xl:w-[50%] ">
             <img
@@ -35,11 +35,11 @@ const About = ({imagesData}) => {
             />
           </figure>
         </div>
-        <article className="relative z-0 flex flex-col  justify-center items-center gap-4 lg:gap-5 text-center sm:max-w-[600px] md:max-w-[700px] lg:mr-[2%] 2xl:mr-[6%] lg:w-[50%]  xl:gap-9 2xl:gap-12 ">
+        <article className="relative z-0 flex flex-col  justify-center items-center gap-4 lg:gap-5 text-center sm:max-w-[600px] md:max-w-[700px] lg:mr-[2%] 2xl:mr-[6%] lg:w-[50%]  xl:gap-7 2xl:gap-9 ">
           <h6 className="font-title text-[2.35rem] sm:text-[2.45rem]  font-bold md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-7xl bg-gradient-to-br from-stone-500 via-stone-300 to-stone-600    inline-block text-transparent bg-clip-text lg:from-stone-600 lg:via-rose-700 lg:to-stone-700">
             SOBRE NOSOTROS
           </h6>
-          <p className="text-about text-stone-400 font-title text-balance text-center px-[8px] font-semibold text-sm xl:text-base xl:px-8 2xl:text-lg lg:text-stone-500  ">
+          <p className="text-about text-stone-400 font-title text-balance text-center px-[8px] font-semibold text-sm xl:text-base xl:px-8 2xl:text-lg lg:text-stone-600  ">
             Ubicados en el corazón de Caballito, nos dedicamos a ofrecer la
             mejor experiencia en tecnología móvil. Contamos con dos locales
             donde te ofrecemos atención personalizada. Nuestro objetivo es
@@ -53,7 +53,7 @@ const About = ({imagesData}) => {
           </p>
 
           <div className="relative">
-            <ul className="text-stone-400 text-[2.5rem] flex gap-8 mt-2 xl:text-5xl xl:gap-[4rem] 2xl:text-6xl 2xl:gap-20 lg:text-rose-600">
+            <ul className="text-stone-400 text-[2.5rem] flex gap-8 mt-2 xl:text-5xl xl:gap-[4rem] 2xl:text-6xl 2xl:gap-20 lg:text-rose-400">
               <li
                 onMouseEnter={() => handleMouseEnter(0)}
                 onMouseLeave={handleMouseLeave}
