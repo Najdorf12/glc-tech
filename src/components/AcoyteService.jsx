1393e0619367594af056acbbeb77eeca079b96d1
Najdorf <agustin.morro@gmail.com>
2024-12-10 17:46:07 -0300

    add styles

--- src/components/AcoyteService.jsx
+++ src/components/AcoyteService.jsx
@@ -25,64 +25,7 @@ const AcoyteService = () => {
             </figure>
           </Link>
         </div>
-      {/*  <picture className="absolute inset-0 w-full  overflow-hidden flex justify-center items-center">
-        <img
-          src={imgAcoyteService}
-          alt="imgAcoyteService"
-          className="w-full h-full object-cover object-center z-40   "
-        />
-      </picture> */}
-
-      {/* <section className="z-50 flex flex-col justify-center items-center mt-6 lg:w-full xl:mt-[3%]">
-       <article className="flex flex-col justify-center items-center text-balance text-center  lg:self-end lg:mr-[6%]">
-          <h6 className="text-4xl bg-gradient-to-t from-stone-300 via-stone-200 to-white bg-clip-text text-transparent inline-block font-title font-semibold xl:text-7xl 2xl:text-8xl">
-            ACOYTESERVICE
-          </h6>
-          <p className="text-xl text-stone-200 font-semibold  bg-gradient-to-t from-stone-300 via-stone-200 to-white bg-clip-text text-transparent inline-block xl:text-3xl xl:mt-1 2xl:text-4xl">
-            SERVICIO TECNICO MULTIMARCA
-          </p>
-          <ul className=" text-stone-700 text-base  flex items-center justify-center gap-5 font-title font-semibold mt-2 xl:text-stone-500 xl:text-lg xl:mt-3 xl:gap-7 2xl:text-xl 2xl:mt-4">
-            <li>NOTEBOOKS</li>
-            <li>CONSOLAS</li>
-            <li>SMARTPHONES</li>
-          </ul>
-        </article> 
-      </section> */}
-
-      {/* <div className="flex justify-center items-center  gap-3 xl:gap-4 2xl:gap-6 absolute left-0 rounded-tr-2xl bottom-0 border-t-[2px] border-r-[2px] bg-zinc-900 border-stone-500 pt-2 px-6 pb-1 lg:pb-3 lg:px-8 z-50 xl:px-12 xl:pt-3 ">
-        <p className="font-title text-lg font-semibold text-stone-300 xl:text-2xl xl:font-medium">Visitar sitio</p>
-        <button className="button3">
-          <a href="https://www.grupolacomunidad.com.ar" target="_blank">
-            <div className="button__circle3">
-              <svg
-                viewBox="0 0 14 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="button__icon"
-                width="14"
-              >
-                <path
-                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-
-              <svg
-                viewBox="0 0 14 15"
-                fill="none"
-                width="14"
-                xmlns="http://www.w3.org/2000/svg"
-                className="button__icon button__icon--copy"
-              >
-                <path
-                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
-          </a>
-        </button>
-      </div> */}
+     
     </div>
   );
 };
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "../api/axios";
 import Card from "../components/Card";
-import Loader from "../components/Loader";
-import { getAllProducts } from "../api/handlers";
 import BannerInfinix from "../components/BannerInfinix";
+import { getAllProducts } from "../api/handlers";
+import imgAcoyteService from "/banneracoyte.png";
+import imgBannerXiaomi from "/bannerxiaomi.png";
+
 
 const btns = [
   "Motorola",
@@ -16,8 +18,8 @@ const btns = [
   "Consolas",
   "Todos",
 ];
+
 const Store = ({ theme, allProducts, arsPrice, setAllProducts }) => {
-  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const hasVisitedHome = sessionStorage.getItem("hasVisitedHome");
 
@@ -32,43 +34,36 @@ const Store = ({ theme, allProducts, arsPrice, setAllProducts }) => {
   }, []);
 
   const getProductsByCategory = async (category) => {
-    setIsLoading(true);
     try {
       const res = await axios.get(`/products/category/${category}`);
       setAllProducts(res.data);
     } catch (error) {
       console.error(error);
-    } /* finally {
-      setIsLoading(false);
-    } */
+    }
   };
 
   const fetchProducts = async () => {
-    /*   setIsLoading(true); */
     try {
       const productsData = await getAllProducts();
       setAllProducts(productsData);
     } catch (error) {
       console.error("Failed to fetch products:", error);
-    } /* finally {
-      setIsLoading(false);
-    } */
+    }
   };
 
   useEffect(() => {
     const savedCategory = sessionStorage.getItem("selectedCategory");
 
     if (!savedCategory || savedCategory === "TODOS") {
-      fetchProducts(); // Asegúrate de que los productos se carguen siempre si no hay una categoría seleccionada
+      fetchProducts();
     } else {
       getProductsByCategory(savedCategory);
     }
   }, []);
 
-  // Función para manejar la selección de categoría
   const searchByCategory = (category) => {
     const str = category.at(0) + category.slice(1).toLowerCase();
-    sessionStorage.setItem("selectedCategory", category); // Guardar la categoría en sessionStorage
+    sessionStorage.setItem("selectedCategory", category);
     if (category === "TODOS") {
       fetchProducts();
     } else {
@@ -76,37 +71,60 @@ const Store = ({ theme, allProducts, arsPrice, setAllProducts }) => {
     }
   };
 
+  // Función para dividir en chunks
+  const chunkArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  };
+
+  // Divide los productos en segmentos de 10
+  const productChunks = chunkArray(allProducts || [], 12);
+
   return (
     <>
       <section
         id="store"
         className="bg-zinc-900 relative w-full pt-12 xl:pt-14 2xl:pt-20"
       >
-        {/* {isLoading && <Loader />} */}
-        <article className="font-title flex flex-col justify-center items-center gap-5  xl:gap-10  ">
-          <p className="text-gray-600 text-7xl xl:text-8xl 2xl:text-9xl font-bold bg-gradient-to-t from-stone-300 via-stone-200 to-white bg-clip-text text-transparent inline-block ">
+        <article className="font-title flex flex-col justify-center items-center gap-5 xl:gap-10">
+          <p className="text-gray-600 text-7xl xl:text-8xl 2xl:text-9xl font-bold bg-gradient-to-t from-stone-300 via-stone-200 to-white bg-clip-text text-transparent inline-block">
             TIENDA
           </p>
           <p className="text-stone-600 text-xl font-medium flex items-center justify-center gap-2 xl:gap-0 xl:text-3xl 2xl:text-4xl">
             BUSCA POR CATEGORÍA
-            <i className="bx bx-log-in-circle text-3xl  text-white xl:text-5xl xl:ml-3"></i>
+            <i className="bx bx-log-in-circle text-3xl text-white xl:text-5xl xl:ml-3"></i>
           </p>
         </article>
-        <div className="flex flex-wrap justify-center items-center gap-y-4 mt-5 gap-x-[2rem]  lg:max-w-[900px] lg:gap-x-6   lg:mx-auto self-center  xl:max-w-[1000px] xl:mt-7 2xl:max-w-[1200px] 2xl:gap-x-7 2xl:px-0 2xl:">
+        <div className="flex flex-wrap justify-center items-center gap-y-4 mt-5 gap-x-[2rem] lg:max-w-[900px] lg:gap-x-6 lg:mx-auto self-center xl:max-w-[1000px] xl:mt-7 2xl:max-w-[1200px] 2xl:gap-x-7 2xl:px-0">
           {btns.map((btn, i) => (
             <button
               onClick={(e) => searchByCategory(e.target?.innerText)}
               key={i}
-              className="py-2 px-6 min-w-36 xl:min-w-44 xl:py-[9px] xl:px-12 hover:scale-105   tracking-wider font-title font-bold text-stone-100  2xl:text-xl bg-gradient-to-tr from-zinc-900 via-rose-800 to-zinc-900 border border-stone-700  rounded-br-xl rounded-tl-xl shadow-sm shadow-gray-500  hover:shadow-white  hover:text-stone-400 duration-500"
+              className="py-2 px-6 min-w-36 xl:min-w-44 xl:py-[9px] xl:px-12 hover:scale-105 tracking-wider font-title font-bold text-stone-100 2xl:text-xl bg-gradient-to-tr from-zinc-900 via-rose-800 to-zinc-900 border border-stone-700 rounded-br-xl rounded-tl-xl shadow-sm shadow-gray-500 hover:shadow-white hover:text-stone-400 duration-500"
             >
               <span>{btn.toUpperCase()}</span>
             </button>
           ))}
         </div>
 
-        <div className="w-full flex flex-wrap justify-center items-center mt-12 gap-y-6 gap-x-3 sm:gap-x-4 lg:gap-x-8 lg:gap-y-12 xl:mt-24 xl:px-[8%]">
-          {allProducts?.map((product, i) => (
-            <Card key={i} product={product} arsPrice={arsPrice} />
+        <div className="w-full flex flex-col items-center mt-12 gap-y-6 xl:mt-24 xl:px-[6%]">
+          {productChunks.map((chunk, index) => (
+            <div key={index} className="w-full flex flex-wrap justify-center items-center gap-y-5 gap-x-3 sm:gap-x-4 lg:gap-x-9 lg:gap-y-14">
+              {chunk.map((product, i) => (
+                <Card key={i} product={product} arsPrice={arsPrice} />
+              ))}
+              {/* Renderiza una imagen después de cada segmento */}
+              <div className="w-full flex justify-center my-8 lg:mb-12">
+                {index % 2 === 0 ? (
+                  <img src={imgAcoyteService} alt="Banner" className="w-full h-full object-contain object-center rounded-sm z-40 lg:w-[85%]" />
+                ) : (
+                  <img src={imgBannerXiaomi} alt="Banner" className="w-full h-full object-contain object-center rounded-sm z-40 lg:w-[85%]" />
+                )}
+              </div>
+            </div>
           ))}
         </div>
       </section>
